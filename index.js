ced9a5ba1a7d107d75a0c0c89ac2a6047ead6610
NyShaun Greene <nyshaungreene@gmail.com>
2023-01-04 10:31:04 -0500

    Second Push - corrected alart

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function greet(name, height, country) {
     console.log('Hello ' + name + ' ' + height + ' ' + country);
-    alert(greet)
+    alert("NyShaun, 5'10, United States")
 }
 
 greet ('NyShaun', '5"10', 'United States' );
